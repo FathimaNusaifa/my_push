74b33b2112eef08a0c9da0b9ccc0a4fbd4e3cad4
aasim <aasim782@gmail.com>
2022-06-08 11:36:59 +0530

    first commit

---
+++ app/screens/SignInScreen.js
@@ -0,0 +1,144 @@
+import {StyleSheet, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {
+  Screen,
+  Block,
+  Typography,
+  Button,
+  TextBox,
+  ToastAndroid,
+} from '../components/index';
+import * as Animatable from 'react-native-animatable';
+import LinearGradient from 'react-native-linear-gradient';
+import {colors} from '../theme';
+import routes from '../navigation/routes';
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const navigation = useNavigation();
+
+  const handleLogin = () => {
+    console.log(email, password);
+    navigation.navigate(routes.WELCOME);
+  };
+
+  const handleHome = () => {
+    console.log(email, password);
+    navigation.navigate(routes.PERSONALINFO);
+  };
+
+  return (
+    <Screen>
+      <StatusBar backgroundColor={colors.secondary} barStyle="light-content" />
+      <LinearGradient
+        colors={[colors.secondary, colors.primary]}
+        style={styles.linearBg}>
+        <Block flex={0.5} center middle>
+          <Typography bold white size={30}>
+            Welcome to UBI!
+          </Typography>
+          <Typography bold white>
+            Sign In
+          </Typography>
+        </Block>
+        <Animatable.View animation="fadeInUpBig" style={styles.animationBlock}>
+          <Block>
+            <Typography black style={styles.label}>
+              Email
+            </Typography>
+            <TextBox
+              email
+              icon="mail"
+              onChangeText={setEmail}
+              autoCapitalize="none"
+              placeholder="Your email"
+            />
+            <Typography black style={styles.label}>
+              Password
+            </Typography>
+            <TextBox
+              icon="lock-closed-sharp"
+              onChangeText={setPassword}
+              autoCapitalize="none"
+              placeholder="Your password"
+              secureTextEntry={true}
+            />
+            <Button gradient shadow onPress={() => {}}>
+              <Typography center white bold size={15}>
+                LOGIN
+              </Typography>
+            </Button>
+            <Button white shadow onPress={() => navigation.goBack()}>
+              <Typography center black bold size={15}>
+                SIGNUP
+              </Typography>
+            </Button>
+          </Block>
+          <Block flex={false} style={styles.forgotBlock}>
+            <Typography
+              center
+              primary
+              bold
+              size={12}
+              onPress={() => navigation.navigate(routes.FORGOTPASSWORD)}>
+              Forgot Password?
+            </Typography>
+          </Block>
+        </Animatable.View>
+      </LinearGradient>
+    </Screen>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  linearBg: {
+    flex: 1,
+  },
+  animationBlock: {
+    flex: 0.5,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingVertical: 30,
+    paddingHorizontal: 30,
+  },
+  headerBlock: {
+    backgroundColor: 'red',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  label: {
+    paddingLeft: 5,
+    marginBottom: 0,
+    fontSize: 13,
+  },
+  forgotBlock: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+});
+
+/**
+ * <View style={styles.container}>
+      <Text>SignInScreen</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
+        <Text>Go Back</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ForgotPassword')}>
+        <Text>Forgot Password</Text>
+      </TouchableOpacity>
+    </View>
+ */
---
+++ app/screens/SignUpScreen.js
@@ -0,0 +1,147 @@
+import {StyleSheet, ScrollView, StatusBar} from 'react-native';
+import React, {useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
+import {Screen, Block, Typography, Button, TextBox} from '../components/index';
+import * as Animatable from 'react-native-animatable';
+import LinearGradient from 'react-native-linear-gradient';
+import routes from '../navigation/routes';
+import {colors} from '../theme/index';
+
+const SignInScreen = () => {
+  const [email, setEmail] = useState(null);
+  const [name, setName] = useState(null);
+  const [phoneNumer, setphoneNumer] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [NIC, setNIC] = useState(null);
+  const [confirmPassword, setconfirmPassword] = useState(null);
+  const navigation = useNavigation();
+
+  const handleSignUp = () => {
+    console.log(email, phoneNumer, NIC, password, confirmPassword);
+    navigation.navigate(routes.WELCOME);
+  };
+
+  return (
+    <Screen>
+      <StatusBar backgroundColor={colors.secondary} barStyle="light-content" />
+      <LinearGradient
+        colors={[colors.secondary, colors.primary]}
+        style={styles.linearBg}>
+        <Block flex={0.2} center middle>
+          <Typography bold white size={30}>
+            Welcome to UBI!
+          </Typography>
+          <Typography bold white>
+            Sign Up
+          </Typography>
+        </Block>
+        <Animatable.View
+          animation="fadeInUp"
+          duration={2000}
+          style={styles.animationBlock}>
+          <ScrollView showsVerticalScrollIndicator={false}>
+            <Typography black style={styles.label}>
+              Email
+            </Typography>
+            <TextBox
+              email
+              icon="mail"
+              onChangeText={setEmail}
+              autoCapitalize="none"
+              placeholder="Your email"
+            />
+
+            <Typography black style={styles.label}>
+              Phone
+            </Typography>
+            <TextBox
+              phone
+              icon="ios-keypad"
+              onChangeText={setphoneNumer}
+              autoCapitalize="none"
+              placeholder="Your phone number"
+            />
+            <Typography black style={styles.label}>
+              NIC
+            </Typography>
+            <TextBox
+              icon="pricetag"
+              onChangeText={setNIC}
+              autoCapitalize="none"
+              placeholder="Your NIC"
+            />
+            <Typography black style={styles.label}>
+              Password
+            </Typography>
+            <TextBox
+              icon="lock-closed-sharp"
+              onChangeText={setPassword}
+              autoCapitalize="none"
+              placeholder="Enter password"
+              secureTextEntry
+            />
+            <Typography black style={styles.label}>
+              Conform Password
+            </Typography>
+            <TextBox
+              icon="lock-closed-outline"
+              onChangeText={setconfirmPassword}
+              autoCapitalize="none"
+              placeholder="Confirm password"
+              secureTextEntry
+            />
+            <Button primary shadow onPress={() => handleSignUp()}>
+              <Typography center white bold size={15}>
+                SIGN UP
+              </Typography>
+            </Button>
+            <Button white shadow onPress={() => navigation.goBack()}>
+              <Typography center black bold size={15}>
+                LOGIN
+              </Typography>
+            </Button>
+          </ScrollView>
+        </Animatable.View>
+      </LinearGradient>
+    </Screen>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  linearBg: {
+    flex: 1,
+  },
+  animationBlock: {
+    flex: 0.8,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    paddingVertical: 30,
+    paddingHorizontal: 20,
+  },
+  formBlock: {
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  headerBlock: {
+    backgroundColor: 'red',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  label: {
+    paddingLeft: 5,
+    marginBottom: 0,
+    fontSize: 13,
+  },
+});
